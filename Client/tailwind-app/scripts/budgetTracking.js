6515e5c403fe106f0962c5b2b7f9a58e2c296979
Adan Butto <adone.1999@gmail.com>
2024-02-12 21:17:37 +0200

    commenting unused scripts

--- Client/tailwind-app/scripts/budgetTracking.js
+++ Client/tailwind-app/scripts/budgetTracking.js
@@ -11,50 +11,50 @@ function displayExpenses(expenses) {
 }
 
 // Event listener for month picker change
-document.getElementById("monthPicker").addEventListener("change", function () {
-  const selectedMonth = this.value;
-  const fakeData = {
-    "2024-02": {
-      // February 2024
-      budget: 2000,
-      expenses: 500,
-      categories: {
-        food: 100,
-        learning: 300,
-        car: 100,
-      },
-    },
-    // Add more months as needed
-  };
+// document.getElementById("monthPicker").addEventListener("change", function () {
+//   const selectedMonth = this.value;
+//   const fakeData = {
+//     "2024-02": {
+//       // February 2024
+//       budget: 2000,
+//       expenses: 500,
+//       categories: {
+//         food: 100,
+//         learning: 300,
+//         car: 100,
+//       },
+//     },
+//     // Add more months as needed
+//   };
 
-  if (fakeData[selectedMonth]) {
-    const { budget, expenses, categories } = fakeData[selectedMonth];
-    const remainingBudget = budget - expenses;
-    const budgetUsagePercentage = (expenses / budget) * 100;
+//   if (fakeData[selectedMonth]) {
+//     const { budget, expenses, categories } = fakeData[selectedMonth];
+//     const remainingBudget = budget - expenses;
+//     const budgetUsagePercentage = (expenses / budget) * 100;
 
-    document.getElementById(
-      "budgetInfo"
-    ).textContent = `In ${selectedMonth}, you have used $${expenses} out of $${budget}. Your remaining budget is $${remainingBudget}.`;
-    document.getElementById("budgetDisplay").classList.remove("hidden");
+//     document.getElementById(
+//       "budgetInfo"
+//     ).textContent = `In ${selectedMonth}, you have used $${expenses} out of $${budget}. Your remaining budget is $${remainingBudget}.`;
+//     document.getElementById("budgetDisplay").classList.remove("hidden");
 
-    const budgetFill = document.getElementById("budgetFill");
-    budgetFill.style.width = `${budgetUsagePercentage}%`;
-    document.getElementById(
-      "percentage"
-    ).textContent = `${budgetUsagePercentage}%`;
+//     const budgetFill = document.getElementById("budgetFill");
+//     budgetFill.style.width = `${budgetUsagePercentage}%`;
+//     document.getElementById(
+//       "percentage"
+//     ).textContent = `${budgetUsagePercentage}%`;
 
-    // Display expenses breakdown
-    displayExpenses(categories);
-    document.getElementById("expensesDisplay").classList.remove("hidden");
-  } else {
-    document.getElementById("budgetInfo").textContent =
-      "No data available for the selected month.";
-    document.getElementById("budgetDisplay").classList.remove("hidden");
-    document.getElementById("expensesDisplay").classList.add("hidden");
-    budgetFill.style.width = `${0}%`;
-    document.getElementById("percentage").textContent = ``;
-  }
-});
+//     // Display expenses breakdown
+//     displayExpenses(categories);
+//     document.getElementById("expensesDisplay").classList.remove("hidden");
+//   } else {
+//     document.getElementById("budgetInfo").textContent =
+//       "No data available for the selected month.";
+//     document.getElementById("budgetDisplay").classList.remove("hidden");
+//     document.getElementById("expensesDisplay").classList.add("hidden");
+//     budgetFill.style.width = `${0}%`;
+//     document.getElementById("percentage").textContent = ``;
+//   }
+// });
 
 // Function to toggle layout based on window width
 // function toggleLayout() {
